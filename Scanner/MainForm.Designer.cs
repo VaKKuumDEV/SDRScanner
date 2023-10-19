032d8f869de37611f1368721578aed50a9a634ec
VaKKuumDEV <fess25channel@gmail.com>
2023-10-20 00:38:56 +0300

    Мне лень писать, я очень спать хочу

--- Scanner/MainForm.Designer.cs
+++ Scanner/MainForm.Designer.cs
@@ -33,6 +33,7 @@
             Label label3;
             Label label4;
             Label label5;
+            Label label6;
             ContentPanel = new TableLayoutPanel();
             tableLayoutPanel2 = new TableLayoutPanel();
             DevicesBox = new ComboBox();
@@ -44,11 +45,14 @@
             AveragePowerBox = new TextBox();
             tableLayoutPanel3 = new TableLayoutPanel();
             SpectrPlot = new ScottPlot.FormsPlot();
+            SignalPlot = new ScottPlot.FormsPlot();
+            AudioDevicesBox = new ComboBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
+            label6 = new Label();
             ContentPanel.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)FreqBox).BeginInit();
@@ -61,10 +65,10 @@
             // 
             label1.AutoSize = true;
             label1.Dock = DockStyle.Top;
-            label1.Location = new Point(3, 0);
+            label1.Location = new Point(3, 62);
             label1.Name = "label1";
             label1.Padding = new Padding(2);
-            label1.Size = new Size(168, 26);
+            label1.Size = new Size(198, 26);
             label1.TabIndex = 0;
             label1.Text = "Устройство:";
             // 
@@ -72,21 +76,21 @@
             // 
             label2.AutoSize = true;
             label2.Dock = DockStyle.Top;
-            label2.Location = new Point(3, 60);
+            label2.Location = new Point(3, 124);
             label2.Name = "label2";
             label2.Padding = new Padding(2);
-            label2.Size = new Size(168, 26);
+            label2.Size = new Size(198, 26);
             label2.TabIndex = 2;
-            label2.Text = "Рабочая частота:";
+            label2.Text = "Частота (кГц):";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Dock = DockStyle.Top;
-            label3.Location = new Point(3, 122);
+            label3.Location = new Point(3, 186);
             label3.Name = "label3";
             label3.Padding = new Padding(2);
-            label3.Size = new Size(168, 26);
+            label3.Size = new Size(198, 26);
             label3.TabIndex = 4;
             label3.Text = "Усиление:";
             // 
@@ -97,7 +101,7 @@
             label4.Location = new Point(3, 0);
             label4.Name = "label4";
             label4.Padding = new Padding(2);
-            label4.Size = new Size(168, 26);
+            label4.Size = new Size(198, 26);
             label4.TabIndex = 5;
             label4.Text = "Samplerate:";
             // 
@@ -108,7 +112,7 @@
             label5.Location = new Point(3, 62);
             label5.Name = "label5";
             label5.Padding = new Padding(2);
-            label5.Size = new Size(168, 48);
+            label5.Size = new Size(198, 26);
             label5.TabIndex = 7;
             label5.Text = "Средняя амплитуда:";
             // 
@@ -125,7 +129,7 @@
             ContentPanel.Name = "ContentPanel";
             ContentPanel.RowCount = 1;
             ContentPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            ContentPanel.Size = new Size(580, 457);
+            ContentPanel.Size = new Size(678, 457);
             ContentPanel.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -133,19 +137,21 @@
             tableLayoutPanel2.AutoSize = true;
             tableLayoutPanel2.ColumnCount = 1;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Controls.Add(label3, 0, 4);
-            tableLayoutPanel2.Controls.Add(label2, 0, 2);
-            tableLayoutPanel2.Controls.Add(label1, 0, 0);
-            tableLayoutPanel2.Controls.Add(DevicesBox, 0, 1);
-            tableLayoutPanel2.Controls.Add(FreqBox, 0, 3);
-            tableLayoutPanel2.Controls.Add(GainBox, 0, 5);
-            tableLayoutPanel2.Controls.Add(ControlButton, 0, 7);
-            tableLayoutPanel2.Controls.Add(AdditionalPanel, 0, 6);
+            tableLayoutPanel2.Controls.Add(AudioDevicesBox, 0, 1);
+            tableLayoutPanel2.Controls.Add(label6, 0, 0);
+            tableLayoutPanel2.Controls.Add(label3, 0, 7);
+            tableLayoutPanel2.Controls.Add(label2, 0, 5);
+            tableLayoutPanel2.Controls.Add(label1, 0, 3);
+            tableLayoutPanel2.Controls.Add(DevicesBox, 0, 4);
+            tableLayoutPanel2.Controls.Add(FreqBox, 0, 6);
+            tableLayoutPanel2.Controls.Add(GainBox, 0, 8);
+            tableLayoutPanel2.Controls.Add(ControlButton, 0, 10);
+            tableLayoutPanel2.Controls.Add(AdditionalPanel, 0, 9);
             tableLayoutPanel2.Dock = DockStyle.Top;
-            tableLayoutPanel2.Location = new Point(406, 0);
+            tableLayoutPanel2.Location = new Point(474, 0);
             tableLayoutPanel2.Margin = new Padding(0);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 8;
+            tableLayoutPanel2.RowCount = 11;
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
@@ -154,7 +160,10 @@
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
-            tableLayoutPanel2.Size = new Size(174, 374);
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.Size = new Size(204, 418);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // DevicesBox
@@ -163,41 +172,41 @@
             DevicesBox.AutoCompleteSource = AutoCompleteSource.ListItems;
             DevicesBox.Dock = DockStyle.Top;
             DevicesBox.FormattingEnabled = true;
-            DevicesBox.Location = new Point(3, 29);
+            DevicesBox.Location = new Point(3, 91);
             DevicesBox.Name = "DevicesBox";
-            DevicesBox.Size = new Size(168, 30);
-            DevicesBox.TabIndex = 1;
+            DevicesBox.Size = new Size(198, 30);
+            DevicesBox.TabIndex = 3;
             // 
             // FreqBox
             // 
             FreqBox.Dock = DockStyle.Top;
             FreqBox.Increment = new decimal(new int[] { 1000, 0, 0, 0 });
-            FreqBox.Location = new Point(3, 89);
-            FreqBox.Maximum = new decimal(new int[] { 1600000000, 0, 0, 0 });
-            FreqBox.Minimum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            FreqBox.Location = new Point(3, 153);
+            FreqBox.Maximum = new decimal(new int[] { 1600000, 0, 0, 0 });
+            FreqBox.Minimum = new decimal(new int[] { 100000, 0, 0, 0 });
             FreqBox.Name = "FreqBox";
-            FreqBox.Size = new Size(168, 30);
+            FreqBox.Size = new Size(198, 30);
             FreqBox.TabIndex = 3;
             FreqBox.ThousandsSeparator = true;
-            FreqBox.Value = new decimal(new int[] { 100000000, 0, 0, 0 });
+            FreqBox.Value = new decimal(new int[] { 102600, 0, 0, 0 });
             // 
             // GainBox
             // 
             GainBox.Dock = DockStyle.Top;
             GainBox.FormattingEnabled = true;
-            GainBox.Location = new Point(3, 151);
+            GainBox.Location = new Point(3, 215);
             GainBox.Name = "GainBox";
-            GainBox.Size = new Size(168, 30);
+            GainBox.Size = new Size(198, 30);
             GainBox.TabIndex = 5;
             // 
             // ControlButton
             // 
             ControlButton.AutoSize = true;
             ControlButton.Dock = DockStyle.Top;
-            ControlButton.Location = new Point(3, 331);
+            ControlButton.Location = new Point(3, 375);
             ControlButton.Name = "ControlButton";
             ControlButton.Padding = new Padding(4);
-            ControlButton.Size = new Size(168, 40);
+            ControlButton.Size = new Size(198, 40);
             ControlButton.TabIndex = 6;
             ControlButton.Text = "Запуск";
             ControlButton.UseVisualStyleBackColor = true;
@@ -212,7 +221,7 @@
             AdditionalPanel.Controls.Add(SamplerateBox, 0, 1);
             AdditionalPanel.Controls.Add(AveragePowerBox, 0, 3);
             AdditionalPanel.Dock = DockStyle.Top;
-            AdditionalPanel.Location = new Point(0, 182);
+            AdditionalPanel.Location = new Point(0, 248);
             AdditionalPanel.Margin = new Padding(0);
             AdditionalPanel.Name = "AdditionalPanel";
             AdditionalPanel.RowCount = 4;
@@ -220,7 +229,9 @@
             AdditionalPanel.RowStyles.Add(new RowStyle());
             AdditionalPanel.RowStyles.Add(new RowStyle());
             AdditionalPanel.RowStyles.Add(new RowStyle());
-            AdditionalPanel.Size = new Size(174, 146);
+            AdditionalPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            AdditionalPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            AdditionalPanel.Size = new Size(204, 124);
             AdditionalPanel.TabIndex = 7;
             // 
             // SamplerateBox
@@ -230,17 +241,17 @@
             SamplerateBox.Maximum = new decimal(new int[] { 1600000000, 0, 0, 0 });
             SamplerateBox.Name = "SamplerateBox";
             SamplerateBox.ReadOnly = true;
-            SamplerateBox.Size = new Size(168, 30);
+            SamplerateBox.Size = new Size(198, 30);
             SamplerateBox.TabIndex = 6;
             SamplerateBox.ThousandsSeparator = true;
             // 
             // AveragePowerBox
             // 
             AveragePowerBox.Dock = DockStyle.Top;
-            AveragePowerBox.Location = new Point(3, 113);
+            AveragePowerBox.Location = new Point(3, 91);
             AveragePowerBox.Name = "AveragePowerBox";
             AveragePowerBox.ReadOnly = true;
-            AveragePowerBox.Size = new Size(168, 30);
+            AveragePowerBox.Size = new Size(198, 30);
             AveragePowerBox.TabIndex = 8;
             // 
             // tableLayoutPanel3
@@ -248,14 +259,15 @@
             tableLayoutPanel3.ColumnCount = 1;
             tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel3.Controls.Add(SpectrPlot, 0, 0);
+            tableLayoutPanel3.Controls.Add(SignalPlot, 0, 1);
             tableLayoutPanel3.Dock = DockStyle.Fill;
             tableLayoutPanel3.Location = new Point(0, 0);
             tableLayoutPanel3.Margin = new Padding(0);
             tableLayoutPanel3.Name = "tableLayoutPanel3";
             tableLayoutPanel3.RowCount = 2;
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 60F));
-            tableLayoutPanel3.Size = new Size(406, 457);
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(474, 457);
             tableLayoutPanel3.TabIndex = 1;
             // 
             // SpectrPlot
@@ -265,21 +277,53 @@
             SpectrPlot.Margin = new Padding(0);
             SpectrPlot.Name = "SpectrPlot";
             SpectrPlot.Padding = new Padding(2);
-            SpectrPlot.Size = new Size(406, 182);
+            SpectrPlot.Size = new Size(474, 228);
             SpectrPlot.TabIndex = 0;
+            // 
+            // SignalPlot
+            // 
+            SignalPlot.Dock = DockStyle.Fill;
+            SignalPlot.Location = new Point(0, 228);
+            SignalPlot.Margin = new Padding(0);
+            SignalPlot.Name = "SignalPlot";
+            SignalPlot.Size = new Size(474, 229);
+            SignalPlot.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Top;
+            label6.Location = new Point(3, 0);
+            label6.Name = "label6";
+            label6.Padding = new Padding(2);
+            label6.Size = new Size(198, 26);
+            label6.TabIndex = 8;
+            label6.Text = "Вывод:";
+            // 
+            // AudioDevicesBox
+            // 
+            AudioDevicesBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            AudioDevicesBox.AutoCompleteSource = AutoCompleteSource.ListItems;
+            AudioDevicesBox.Dock = DockStyle.Top;
+            AudioDevicesBox.FormattingEnabled = true;
+            AudioDevicesBox.Location = new Point(3, 29);
+            AudioDevicesBox.Name = "AudioDevicesBox";
+            AudioDevicesBox.Size = new Size(198, 30);
+            AudioDevicesBox.TabIndex = 9;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(584, 461);
+            ClientSize = new Size(682, 461);
             Controls.Add(ContentPanel);
             Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(4);
-            MinimumSize = new Size(600, 500);
+            MinimumSize = new Size(700, 500);
             Name = "MainForm";
             Padding = new Padding(2);
             Text = "Scanner";
+            WindowState = FormWindowState.Maximized;
             ContentPanel.ResumeLayout(false);
             ContentPanel.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
@@ -305,5 +349,7 @@
         private TableLayoutPanel AdditionalPanel;
         private NumericUpDown SamplerateBox;
         private TextBox AveragePowerBox;
+        private ScottPlot.FormsPlot SignalPlot;
+        private ComboBox AudioDevicesBox;
     }
 }
--- Scanner/MainForm.cs
+++ Scanner/MainForm.cs
@@ -2,6 +2,8 @@ using SDRSharp.RTLSDR;
 using SDRSharp.Radio;
 using Scanner.Audio;
 using System.Numerics;
+using System.Media;
+using SDRSharp.Radio.PortAudio;
 
 namespace Scanner
 {
@@ -9,9 +11,11 @@ namespace Scanner
     {
         private static RtlDevice? IO { get; set; } = null;
         private List<DeviceDisplay> DevicesList { get; } = new();
+        private List<AudioDevice> AudioDevices { get; } = new();
         private WorkingStatuses Status { get; set; } = WorkingStatuses.NOT_INIT;
         private DateTime SignalTime { get; set; } = DateTime.Now;
-        private StreamPlayer Player { get; set; }
+        private List<double> FrequesList { get; set; } = new();
+        private WavePlayer? Player { get; set; }
 
         public enum WorkingStatuses
         {
@@ -20,15 +24,20 @@ namespace Scanner
             STOPPED,
         };
 
-        public MainForm()
+        unsafe public MainForm()
         {
             InitializeComponent();
             AdditionalPanel.Hide();
 
             SpectrPlot.Plot.Title("Осциллограмма");
-            SpectrPlot.Plot.XLabel("Время (мс)");
-            SpectrPlot.Plot.YLabel("Амплитуда");
+            SpectrPlot.Plot.XLabel("Частота (Гц)");
+            SpectrPlot.Plot.YLabel("Мощность (дБ)");
             SpectrPlot.Refresh();
+
+            SignalPlot.Plot.Title("Сигнал");
+            SignalPlot.Plot.XLabel("Время (мс)");
+            SignalPlot.Plot.YLabel("Амплитуда");
+            SignalPlot.Refresh();
 
             DevicesBox.SelectedIndexChanged += new((sender, args) =>
             {
@@ -52,31 +61,29 @@ namespace Scanner
                 }
             });
 
+            AudioDevicesBox.SelectedIndexChanged += new((sender, args) =>
+            {
+                int index = AudioDevicesBox.SelectedIndex;
+                if (index != -1 && index < AudioDevices.Count)
+                {
+                    if (Player == null) return;
+
+                    AudioDevice device = AudioDevices[index];
+                    Player = new(device.Index, 44100, 100, (buffer, length) =>
+                    {
+                        
+                    });
+                }
+            });
+
             ControlButton.Click += new((sender, args) =>
             {
                 if (Status == WorkingStatuses.STARTED) Stop();
                 else if (Status == WorkingStatuses.STOPPED) Start();
             });
 
-            Player = new();
-            Player.PlayAsync();
-            Random rnd = new();
-
-            Thread thread = new(new ThreadStart(() =>
-            {
-                while (!IsDisposed)
-                {
-                    var freq = 0.01 * rnd.Next(10);
-                    for (int i = 0; i < 1000; i++)
-                    {
-                        var v = (short)(Math.Sin(freq * i * Math.PI * 2) * short.MaxValue);
-                        Player.Write(v);
-                    }
-                }
-            }));
-            thread.Start();
-
             LoadDevicesList();
+            LoadAudioDevicesList();
         }
 
         private void LoadDevicesList()
@@ -89,6 +96,19 @@ namespace Scanner
             foreach (DeviceDisplay device in DevicesList) DevicesBox.Items.Add("(#" + device.Index + ") " + device.Name);
 
             Status = WorkingStatuses.STOPPED;
+            if (DevicesList.Count > 0) DevicesBox.SelectedIndex = 0;
+        }
+
+        private void LoadAudioDevicesList()
+        {
+            List<AudioDevice> devices = AudioDevice.GetDevices(DeviceDirection.Output);
+            AudioDevices.Clear();
+            AudioDevices.AddRange(devices);
+
+            AudioDevicesBox.Items.Clear();
+            foreach (AudioDevice device in AudioDevices) AudioDevicesBox.Items.Add("(#" + device.Index + ") " + device.Name);
+
+            if (AudioDevices.Count > 0) AudioDevicesBox.SelectedIndex = 0;
         }
 
         private void Stop()
@@ -105,7 +125,7 @@ namespace Scanner
             if (IO != null)
             {
                 SignalTime = DateTime.Now;
-                uint freq = (uint)FreqBox.Value;
+                uint freq = (uint)FreqBox.Value * 1000;
                 int gain = 0;
 
                 if (GainBox.SelectedIndex != -1) gain = IO.SupportedGains[GainBox.SelectedIndex];
@@ -113,6 +133,11 @@ namespace Scanner
                 IO.Frequency = freq;
                 IO.Gain = gain;
                 IO.Start();
+                IO.UseTunerAGC = true;
+
+                FrequesList = new(new double[8192]);
+                double fSampleRate = IO.Samplerate / FrequesList.Count;
+                for (int i = 0; i < FrequesList.Count; i++) FrequesList[i] = IO.Frequency - (IO.Samplerate / 2) + (i * fSampleRate);
 
                 ControlButton.Text = "Остановить";
                 SamplerateBox.Value = IO.Samplerate;
@@ -127,22 +152,40 @@ namespace Scanner
         private unsafe void IO_SamplesAvailable(object sender, SamplesAvailableEventArgs e)
         {
             float* power = stackalloc float[e.Length];
+            Fourier.InverseTransform(e.Buffer, e.Length);
             Fourier.SpectrumPower(e.Buffer, power, e.Length);
 
-            if ((DateTime.Now - SignalTime).TotalSeconds >= 1)
+            float* audio = stackalloc float[e.Length];
+            FmDetector det = new()
             {
-                List<double> fftPower = new();
-                for (int i = 0; i < e.Length; i++) fftPower.Add(power[i]);
-                double averagePower = fftPower.Average();
+                Mode = FmMode.Wide,
+                SampleRate = IO.Samplerate
+            };
+            det.Demodulate(e.Buffer, audio, e.Length);
+
+            if ((DateTime.Now - SignalTime).TotalMilliseconds >= 100)
+            {
+                List<double> fftPower = new(new double[e.Length]);
+                for (int i = 0; i < e.Length; i++) fftPower[i] = power[i];
+
+                List<double> fftAudio = new(new double[e.Length]);
+                for (int i = 0; i < e.Length; i++) fftAudio[i] = audio[i];
+                double[] filtered = FftSharp.Filter.LowPass(fftAudio.ToArray(), IO.Samplerate, maxFrequency: 2000);
 
                 BeginInvoke(() =>
                 {
                     SpectrPlot.Plot.Clear();
-                    SpectrPlot.Plot.AddSignal(fftPower.ToArray());
-                    SpectrPlot.Plot.AddHorizontalLine(averagePower, Color.Green);
+                    SpectrPlot.Plot.AddSignalXY(FrequesList.ToArray(), fftPower.ToArray());
+                    SpectrPlot.Plot.AddVerticalLine(FrequesList[FrequesList.Count / 2], Color.Red);
+                    SpectrPlot.Plot.SetAxisLimitsY(-130, 20);
                     SpectrPlot.Refresh();
 
-                    AveragePowerBox.Text = averagePower.ToString();
+                    SignalPlot.Plot.Clear();
+                    SignalPlot.Plot.AddSignal(filtered);
+                    SignalPlot.Plot.SetAxisLimitsY(-1E-5, 1E-5);
+                    SignalPlot.Refresh();
+
+                    AveragePowerBox.Text = "NaN";
                 });
                 SignalTime = DateTime.Now;
             }
