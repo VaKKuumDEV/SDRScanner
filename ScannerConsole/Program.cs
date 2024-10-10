799d3d39ad87d1187692557347ac779864bcba76
VaKKuumDEV <fess25channel@gmail.com>
2024-10-10 21:06:22 +0300

    Консольная версия работает на Windows

--- ScannerConsole/Program.cs
+++ ScannerConsole/Program.cs
@@ -1,24 +1,43 @@
-﻿using FftSharp;
-using RtlSdrManager;
-using RtlSdrManager.Types;
+﻿using SDRSharp.Radio;
+using SDRSharp.RTLSDR;
 
-var manager = RtlSdrDeviceManager.Instance;
-
-Console.WriteLine("Count devices: " + manager.CountDevices);
-Console.WriteLine("Devices: " + string.Join(", ", manager.Devices.Select(d => "(" + d.Value.Index + ") " + d.Value.Name)));
-
-manager.OpenManagedDevice(0, "my-rtl-sdr");
-manager["my-rtl-sdr"].CenterFrequency = new Frequency { MHz = 433 };
-manager["my-rtl-sdr"].SampleRate = new Frequency { MHz = 2 };
-manager["my-rtl-sdr"].TunerGainMode = TunerGainModes.AGC;
-manager["my-rtl-sdr"].AGCMode = AGCModes.Enabled;
-manager["my-rtl-sdr"].ResetDeviceBuffer();
-
-manager["my-rtl-sdr"].StartReadSamplesAsync();
-manager["my-rtl-sdr"].SamplesAvailable += new((sender, args) =>
+DeviceDisplay[] devices = DeviceDisplay.GetActiveDevices();
+if (devices.Length > 0)
 {
-    var samples = manager["my-rtl-sdr"].GetSamplesFromAsyncBuffer(args.SampleCount);
-});
+    Console.WriteLine("Devices:");
+    for (int i = 0; i < devices.Length; i++) Console.WriteLine(string.Format("Device {0}: {1}", devices[i].Index, devices[i].Name));
 
-Console.Read();
-manager.CloseAllManagedDevice();
+    Console.WriteLine();
+    Console.Write("Enter index: ");
+
+    int deviceIndex = int.Parse(Console.ReadLine() ?? "0");
+    RtlDevice device = new(devices[deviceIndex].Index);
+    device.SamplesAvailable += IO_SamplesAvailable;
+    device.Frequency = 433950000u;
+
+    device.Start();
+
+    Console.Read();
+    device.Stop();
+}
+else
+{
+    Console.WriteLine("No devices found");
+}
+
+static unsafe void IO_SamplesAvailable(object sender, SamplesAvailableEventArgs e)
+{
+    if (sender is RtlDevice device)
+    {
+        Fourier.ForwardTransform(e.Buffer, e.Length);
+
+        float[] power = new float[e.Length];
+        float[] simpleAveraged = new float[e.Length];
+        fixed (float* srcPower = power)
+        {
+            Fourier.SpectrumPower(e.Buffer, srcPower, e.Length, device.Gain);
+        }
+
+        Console.WriteLine("Average: " + power.Average());
+    }
+}
