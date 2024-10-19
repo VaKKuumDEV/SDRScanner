21e13ed67558929e6f4536f7a857e07c873eb963
VaKKuumDEV <fess25channel@gmail.com>
2024-10-20 00:15:07 +0300

    Начато переписывание на Avalonia для кросс-платформенности UI

---
+++ ScannerUI/MainSingleView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ScannerUI;
+
+public partial class MainSingleView : UserControl
+{
+    public MainSingleView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ScannerUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace ScannerUI
+{
+    internal class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            var builder = BuildAvaloniaApp();
+            if (args.Contains("--drm"))
+            {
+                SilenceConsole();
+                // By default, Avalonia will try to detect output card automatically.
+                // But you can specify one, for example "/dev/dri/card1".
+                return builder.StartLinuxDrm(args: args, card: null, scaling: 1.0);
+            }
+
+            return builder.StartWithClassicDesktopLifetime(args);
+        }
+
+        private static void SilenceConsole()
+        {
+            new Thread(() =>
+            {
+                Console.CursorVisible = false;
+                while (true)
+                    Console.ReadKey(true);
+            })
+            { IsBackground = true }.Start();
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
+    }
+}
